599840484a29a61941a3509ce32046dfb9d75244
SKYZAP <60080933+SKYZAP@users.noreply.github.com>
2020-02-24 11:39:12 +0800

    Add files via upload

---
+++ leb.R
@@ -0,0 +1,44 @@
+library(shiny)
+
+ui <- fluidPage(  
+  titlePanel("Uploading Files"),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file1", "Choose CSV File",
+                multiple = TRUE,
+                accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
+      tags$hr(),
+      checkboxInput("header", "Header", TRUE),
+      radioButtons("sep", "Separator",
+                   choices = c(Comma = ",", Semicolon = ";", Tab = "\t"),
+                   selected = ","),
+      radioButtons("quote", "Quote",
+                   choices = c(None = "", "Double Quote" = '"', "Single Quote" = "'"),
+                   selected = '"'),
+      tags$hr(),
+      radioButtons("disp", "Display",
+                   choices = c(Head = "head", All = "all"),
+                   selected = "head")
+    ),
+    mainPanel(
+      tableOutput("contents") 
+    )    
+  )
+)
+server <- function(input, output) {
+  output$contents <- renderTable({
+    req(input$file1)
+    df <- read.csv(input$file1$datapath,
+                   header = input$header,
+                   sep = input$sep,
+                   quote = input$quote)
+    if(input$disp == "head") {
+      return(head(df))
+    }
+    else {
+      return(df)
+    }
+  })  
+}
+
+shinyApp(ui = ui, server = server)
